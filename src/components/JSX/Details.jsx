f54d45a5ad5cd2bfc4e932be4248442f8e2cc82b
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-04-10 20:17:23 +0600

    details apply cart finish

--- src/components/JSX/Details.jsx
+++ src/components/JSX/Details.jsx
@@ -24,15 +24,47 @@ const Details = () => {
                 </div>
             <img src='/src/assets/All Images/Vector-1.png' alt="" />
             </div>
-            <div className='flex'>
-                <div className='w-[50%]'>
+            <div className='flex w-10/12 mx-auto border-2 p-5'>
+                <div className='w-[60%] mx-5'>
                     <h1><span className='font-bold'>Job Description : </span> {details.jobDesc}</h1>
                     <h1><span className='font-bold'>Job Responsibility : </span> {details.jobRes}</h1>
                     <h1><span className='font-bold'>Educational Requirements : <br/></span>{details.education}</h1>
                     <h1><span className='font-bold'>Experiences : <br/></span>{details.experiences}</h1>
                 </div>
-                <div className='w-[50%]'>
-ghh
+                <div className='w-[40%] mx-5 flex justify-center'>
+                    <div className='w-full info p-5 items-center justify-between'>
+                    <h1 className='border-b border-gray-500 w-full font-bold pb-3 text-lg'>Job Details</h1>
+
+                    <div className='w-full my-2'>
+                        <div className='flex w-full items-center'>
+                        <img className='my-2' src={details.salaryIcon} alt="" />
+                        <p className='my-2 ml-2'><strong>Salary :</strong> {details.salary}</p>
+                        </div>
+                        <div className='flex w-full items-center '>
+                            <img className='my-2' src={details.jobIcon} alt="" />
+                            <p className='my-2 ml-2'><strong>Job Title :</strong> {details.title}</p>
+                        </div>
+                    </div>
+                    
+                    <div className='mt-5'>
+                        <h1 className='border-b border-gray-500 w-full font-bold pb-3 text-lg'>Contact Information</h1>
+                        <div>
+                            <div className='flex items-center w-full'>
+                                <img className='my-2' src={details.phoneIcon} alt="" />
+                                <p className='my-2 ml-2'><strong>Phone : </strong>{details.phone}</p>
+                            </div>
+                            <div className='flex items-center w-full'>
+                                <img className='my-2' src={details.mailIcon} alt="" />
+                                <p className='my-2 ml-2'><strong>Email : </strong> {details.email}</p>
+                            </div>
+                            <div className='flex items-center w-full'>
+                                <img className='my-2' src={details.addressIcon} alt="" />
+                                <p className='my-2 ml-2'><strong>Location : </strong> {details.address}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
                 </div>
             </div>
         </div>
